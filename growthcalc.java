f9f8dd7f56cee654630ca2a3b7ea50f7dad68aeb
Chandan Agrahar <56246770+chandan-agrahar@users.noreply.github.com>
2020-07-24 12:37:34 +0530

    Add files via upload

---
+++ growthcalc.java
@@ -0,0 +1,31 @@
+/* PROJECT 1: Youtube Growth Calculator */
+//Name: Mr. M Chandan Agrahar 
+//USN: 1RR17CS070
+//Semester: 7th
+//College: Rajarajeswari College Of Engineering
+//Date: 16-Jul-2020
+
+package intern;
+import java.util.Scanner;
+public class growthcalc 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("****Welcome to Youtube subscriber count calculator****");
+		System.out.println("Provide the no. of subscribers you have at present(a): ");//a=variable used for growth
+		Scanner sc= new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println("Enter the growth rate(r): ");//r=variable used for growth rate
+		double r = sc.nextDouble();
+		System.out.println("Time Interval in days(x): ");//x=variable used for time interval
+		int x = sc.nextInt();
+		System.out.println("subscriber count would be:");
+		System.out.println(SubGrowth(a,r,x));
+		sc.close();
+	}
+	
+	public static int SubGrowth(int a, double r, int x)
+	{
+		return (int)(a*Math.pow((1+r),x));
+	}
+}
